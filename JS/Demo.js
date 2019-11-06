69092132db988db84cc411a2c077ca078e2ed145
unknown <cliu.jonathan@gmail.com>
2019-11-06 10:44:34 -0500

    Added comments to control flow demo

--- JS/Demo.js
+++ JS/Demo.js
@@ -9,6 +9,7 @@ function() {
 
 function demohelper(number)
 {
+    //displays all numbers from 0 to number, exclusive of number
     i=0;
     do{
         console.log(i);
@@ -17,11 +18,11 @@ function demohelper(number)
 }
 function demo(number,option,val){
     switch(option){
-        case 1:
+        case 1: //roll val number of 'number' sided die. demo(20,1,3) = 3d20
             for(i=1;i<=val;i++)
                 console.log(Math.floor(Math.random()*number));
         break;
-        case 2:
+        case 2: //display all numbers from 1 to number that are divisible by val
             i=1;
             while(i<=number){
                 if(i%val==0)
@@ -29,7 +30,7 @@ function demo(number,option,val){
                 i++;
             }
         break;
-        case 3:
+        case 3: //display all elements of a list passed as val. Repeat 'number' amount of times
             for(i=0;i<number;i++)
                 for(element of val)
                     console.log(element);
