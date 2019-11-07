0008673276a0ebae67dbae2f74180e2a7bcb1e85
LiuCJonathan <cliu.jonathan@gmail.com>
2019-11-07 15:37:09 -0500

    Javascript challenges planning work

--- JS/JavascriptChallenges.js
+++ JS/JavascriptChallenges.js
@@ -1,5 +1,134 @@
-function fib(number){
+Define function: fib(n) 
+Return the nth number in the fibonacci sequence.*/
+
+function fib(n) {
+
+}
+
+/*2. Bubble Sort
+Define function: bubbleSort(numArray)
+Use the bubble sort algorithm to sort the array.
+Return the sorted array.*/
+
+function bubbleSort(numArray) {
+
+}
+
+/*3. Reverse String
+Define function: reverseStr(someStr)
+Reverse and return the String.*/
+
+function reverseString(someStr) {
+
+}
+
+/*4. Factorial
+Define function: factorial(someNum)
+Use recursion to compute and return the factorial of someNum.*/
+
+function factorial(someNum) {
+
+}
+
+/*5. Substring
+Define function substring(someStr, length, offset)
+Return the substring contained between offset and (offset + length) inclusively.
+If incorrect input is entered, use the alert function and describe why the input was incorrect.*/
+
+function substring(someStr, length, offset) {
+
+}
+
+/*6. Even Number
+Define function: isEven(someNum)
+Return true if even, false if odd.
+Do not use % operator.*/
+
+function isEven(someNum) {
     
+}
+
+/*7. Palindrome
+Define function isPalindrome(someStr)
+Return true if someStr is a palindrome, otherwise return false*/
+
+function isPalindrome(someStr) {
+
+}
+
+/*8. Shapes
+Define function: printShape(shape, height, character)
+shape is a String and is either "Square", "Triangle", "Diamond".
+height is a Number and is the height of the shape. Assume the number is odd.
+character is a String that represents the contents of the shape. Assume this String contains just one character.
+Use a switch statement to determine which shape was passed in.
+Use the console.log function to print the desired shape.
+Example for printShape("Square", 3, "%");
+%%%
+%%%
+%%%
+Example for printShape("Triangle", 3, "$");
+$
+$$
+$$$
+Example for printShape("Diamond", 5, "*");
+  *
+ ***
+*****
+ ***
+  * */
+
+function printShape(shape, height, character) {
+
+}
+
+/*9. Object literal
+Define function traverseObject(someObj)
+Print every property and it's value.*/
+
+function traverseObject(someObj) {
+
+}
+
+/*10. Delete Element
+Define function deleteElement(someArr)
+Print length
+Delete the third element in the array.
+Print length
+The lengths should be the same.*/
+
+function deleteElement(someDelArr) {
+
+}
+
+/*11. Splice Element
+Define function spliceElement(someArr)
+Print length
+Splice the third element in the array.
+Print length
+The lengths should be one less than the original length.*/
+
+function spliceElement(someArr) {
+    
+}
+
+/*12. Defining an object using a constructor
+Define a function Person(name, age)
+The following line should set a Person object to the variable john:
+	var john = new Person("John", 30);*/
+
+function Person(name, age) {
+
+}
+
+
+/*13. Defining an object using an object literal
+Define function getPerson(name, age)
+The following line should set a Person object to the variable john:
+    var john = getPerson("John", 30);*/
+
+function getPerson(name, age) {
+
 }
 function UpdateTime()
 {
@@ -7,3 +136,12 @@ function UpdateTime()
     document.querySelector("#timestamp").innerHTML = day.toLocaleTimeString();
 }
 setInterval(UpdateTime,1000);
+
+/*15.  Descending order
+Your task is to make a function that can take any non-negative 
+integer as a argument and return it with its digits in descending 
+order. Essentially, rearrange the digits to create the highest possible number.*/
+
+function descOrder(n) {
+
+}
