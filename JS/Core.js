024c2a647074c17690398ce4ab8037b87d65943c
LiuCJonathan <cliu.jonathan@gmail.com>
2019-11-11 14:17:11 -0500

    Added challenges containers and selector functionality

--- JS/Core.js
+++ JS/Core.js
@@ -21,4 +21,41 @@ function navbarIconMouseover(){
 function dropdownClick(event)
 {
     document.querySelector(".dropdownButton").innerHTML=event.target.innerHTML;
+    document.querySelector(".dropdown").classList.remove('show')
+    setActiveChallenge(event.target.innerHTML);
+}
+function setActiveChallenge(challengeName)
+{
+    let challenges=document.getElementsByClassName("challengeContainer");
+    for (i=0;i<challenges.length;i++)
+        challenges[i].style.display="none";
+    switch(challengeName){
+        case "Fibonacci Seq":
+            document.querySelector("#fibContainer").style.display="block";
+            break;
+        case "Bubble Sort":
+            document.querySelector("#bubSortContainer").style.display="block";
+        break;
+        case "Reverse String":
+            document.querySelector("#revStrContainer").style.display="block";
+            break;
+        case "Factorial":
+            document.querySelector("#factoriContainer").style.display="block";
+            break;
+        case "Sub-string":
+            document.querySelector("#subStrContainer").style.display="block";
+            break;
+        case "Is Even":
+            document.querySelector("#isEvenContainer").style.display="block";
+            break;
+        case "Is Palindrome":
+            document.querySelector("#isPalinContainer").style.display="block";
+            break;
+        case "Object Literal":
+            document.querySelector("#objLitContainer").style.display="block";
+            break;
+        case "Desc Order":
+            document.querySelector("#descOrdContainer").style.display="block";
+            break;
+    }
 }
